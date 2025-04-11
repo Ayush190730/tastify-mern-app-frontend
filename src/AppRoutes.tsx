72ca6db335a76de6547709b4283b2a439d91a9df
Ayush190730 <148128641+Ayush190730@users.noreply.github.com>
2025-04-11 16:21:02 +0530

    first commit

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,38 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import HomePage from "./Pages/Homepage";
+import AuthCallbackPage from "./Pages/AuthCallbackPage";
+import UserProfilePage from "./Pages/UserProfilePage";
+import ProtectedRoute from "./Auth/ProtectedRoute";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            {" "}
+            <HomePage />{" "}
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route element={<ProtectedRoute/>}>
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            {" "}
+            <UserProfilePage />
+          </Layout>
+        }
+      />
+      </Route>
+      
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
